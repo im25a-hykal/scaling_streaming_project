477dff74a24badfa3399d95e08c87dd3ff30a6de
im25a-hykal <hykal@bzz.ch>
2025-12-18 08:51:01 +0100

    old version

--- receiver.py
+++ receiver.py
@@ -6,17 +6,18 @@ port = 6769
 file_name = "temporary.txt"
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((host, port))
 server_socket.listen(5)
 
 conn, addr = server_socket.accept()
 
 with open(file_name, "wb") as f:
-        while True:
-            data = conn.recv(1024)
-            f.write(data)
-            if not data:
-                break
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        f.write(data)
 
 print("received data")
 conn.close()
--- sender.py
+++ sender.py
@@ -8,12 +8,9 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((server_ip, port))
 
 with open(sending_file, 'rb') as f:
-    while True:
+    data = f.read(1024)
+    while data:
+        client_socket.sendall(data)
         data = f.read(1024)
-        while data:
-            client_socket.send(data)
-            data = f.read(1024)
-        if not data:
-            break
 
 client_socket.close()
