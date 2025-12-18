9f2fd24d29850dd0654d1145133e842391a6f4ae
im25a-hykal <hykal@bzz.ch>
2025-12-18 08:59:12 +0100

    old version

--- sender.py
+++ sender.py
@@ -1,5 +1,5 @@
 import socket
-import keyboard
+import os
 server_ip = '192.168.178.77'
 port = 6769
 sending_file = 'sending_file.txt'
@@ -7,6 +7,10 @@ sending_file = 'sending_file.txt'
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((server_ip, port))
 
+if not os.path.exists(sending_file):
+    with open(sending_file, 'w') as f:
+        f.write("Dies ist eine automatisch erstellte Test-Datei.")
+    print(f"Datei {sending_file} wurde neu erstellt.")
 with open(sending_file, 'rb') as f:
     data = f.read(1024)
     while data:
